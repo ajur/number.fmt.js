065a1bab100c55999098914528ed3028f09579f9
Adam Jurczyk <ajur.pl@gmail.com>
2011-12-12 21:23:25 +0100

    fixed problems with factorials, all tests passed

--- number.fmt.js
+++ number.fmt.js
@@ -4,29 +4,42 @@
  * dec -> decimal digits (after period, if 0, no decimals at all)
  * curr -> currency string (putted after number)
  * pnt -> decimal point char
- * sep -> full tousends separator
+ * sep -> full thousands separator
  * sepn -> number of digits to separate with 'sep', defaults to '3'
  */
 Number.prototype.fmt = function(dec, curr, pnt, sep, sepn){
+    //simple toString for all non finite numbers
     if(!isFinite(this)) return this.toString();
-    var dec=+(dec||0), curr=curr||'', pnt=pnt||'.', 
-        sep=sep||' ', sepn=+(sepn||3),
-        sn = ''+Math.round(Math.abs(this) * Math.pow(10,dec)),
-        snd = sn.slice(sn.length-dec), i,v=[];
-    sn = sn.slice(0,sn.length-dec);
-    //if(sn.length===0) sn='0';
+    
+    var dec=+(dec||0), //decimal digits
+        curr=curr||'', //currency
+        pnt=pnt||'.', //decimal point char
+        sep=sep||' ', //thousands separator
+        sepn=+(sepn||3), //sep every sepn digits
+        sn = ''+Math.round(Math.abs(this) * Math.pow(10,dec)), //stringify rounded number
+        snd,i,v=[]; //output array
+    
+    console.log('\nnum ', this.toString(), ' dec ', dec);
+
+    console.log('sn ',sn);
+    
+    for(i = sn.length-dec; i<=0; ++i){
+        sn = '0'+sn;
+    }
+    
+    snd = sn.slice(sn.length-dec); //fractional part
+    sn = sn.slice(0,sn.length-dec); //integral part
+    
+    console.log('sn ',sn, ' snd ',snd);
+    
     for(i=sn.length%sepn ; i<sn.length ; i+=sepn){
         v.push(sep,sn.slice(i,i+sepn));
     }
-    if(sn.length===0) v.unshift('0');
-    else if(sn.length%sepn>0) v.unshift(sn.slice(0,sn.length%sepn));
+    if(sn.length%sepn>0) v.unshift(sn.slice(0,sn.length%sepn));
     else v.shift();
     if(this<0) v.unshift('-');
     if(dec>0){
         v.push(pnt,snd);
-        for(i=snd.length-dec ; i>0 ; --i){
-            v.push('0');
-        }
     }
     if(curr.length>0){
         v.push(' ',curr);
